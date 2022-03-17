d3171992c3870828d78a4735e92c4b2f3717834c
yujin113 <claire9585@sookmyung.ac.kr>
2022-03-17 12:34:47 +0900

    initial commit

---
+++ src/main/java/server/sookdak/SookdakApplication.java
@@ -0,0 +1,13 @@
+package server.sookdak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SookdakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SookdakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/server/sookdak/SookdakApplicationTests.java
@@ -0,0 +1,13 @@
+package server.sookdak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SookdakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
