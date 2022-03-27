b5cfaf3d6c9aa77eb1bf4d16bcab13b9c49cdfc7
yujin113 <claire9585@sookmyung.ac.kr>
2022-03-27 13:22:07 +0900

    [feat] 로그인/회원가입, 토큰 재발급, 유저 본인 조회 API (#3)

---
+++ src/main/java/server/sookdak/api/UserApi.java
@@ -0,0 +1,46 @@
+package server.sookdak.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import server.sookdak.dto.TokenDto;
+import server.sookdak.dto.req.TokenRequestDto;
+import server.sookdak.dto.res.TokenResponse;
+import server.sookdak.dto.res.UserResponse;
+import server.sookdak.dto.res.UserResponseDto;
+import server.sookdak.dto.req.UserRequestDto;
+import server.sookdak.service.UserService;
+
+import javax.validation.Valid;
+
+import static server.sookdak.constants.SuccessCode.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class UserApi {
+
+    private final UserService userService;
+
+    @GetMapping("/me")
+    public ResponseEntity<UserResponseDto> getMyMemberInfo() {
+        UserResponseDto userResponseDto = userService.getMyInfo();
+        return UserResponse.toResponse(USER_INFO_SUCCESS, userResponseDto);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenResponse> login(@Valid @RequestBody UserRequestDto userRequestDto) {
+        TokenDto tokenDto = userService.login(userRequestDto);
+        if (tokenDto == null) {
+            return TokenResponse.toResponse(SIGNUP_SUCCESS, null);
+        }
+
+        return TokenResponse.toResponse(LOGIN_SUCCESS, tokenDto);
+    }
+
+    @PostMapping("/reissue")
+    public ResponseEntity<TokenResponse> reissue(@Valid @RequestBody TokenRequestDto tokenRequestDto) {
+        TokenDto tokenDto = userService.reissue(tokenRequestDto);
+        return TokenResponse.toResponse(REISSUE_SUCCESS, tokenDto);
+    }
+}
