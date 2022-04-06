9c62cc463ea7b7f5b3b6d61a299986083e7eef00
yujin113 <claire9585@sookmyung.ac.kr>
2022-04-06 22:32:03 +0900

    [feat] 게시글 목록 조회 repository 메서드 추가 (#21)

--- src/main/java/server/sookdak/repository/PostImageRepository.java
+++ src/main/java/server/sookdak/repository/PostImageRepository.java
@@ -1,7 +1,10 @@
 package server.sookdak.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import server.sookdak.domain.Post;
 import server.sookdak.domain.PostImage;
 
 public interface PostImageRepository extends JpaRepository<PostImage, Long> {
+
+    boolean existsByPost(Post post);
 }
