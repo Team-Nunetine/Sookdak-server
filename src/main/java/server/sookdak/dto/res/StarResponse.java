a015b576f62785bbd2b8363367f676d406a11230
yujin113 <claire9585@sookmyung.ac.kr>
2022-04-07 10:47:30 +0900

    [feat] 게시판 즐겨찾기 응답 dto 생성 (#15)

---
+++ src/main/java/server/sookdak/dto/res/StarResponse.java
@@ -0,0 +1,20 @@
+package server.sookdak.dto.res;
+
+import org.springframework.http.ResponseEntity;
+import server.sookdak.constants.SuccessCode;
+import server.sookdak.dto.BaseResponse;
+
+public class StarResponse extends BaseResponse {
+
+    private StarResponse(Boolean success, String message) {
+        super(success, message);
+    }
+
+    public static StarResponse of(Boolean success, String message) {
+        return new StarResponse(success, message);
+    }
+
+    public static ResponseEntity<StarResponse> newResponse(SuccessCode code) {
+        return new ResponseEntity<>(StarResponse.of(true, code.getMessage()), code.getStatus());
+    }
+}
