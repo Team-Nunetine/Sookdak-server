6eb97849733f7386b78f4aab18e58b5ba7548324
yujin113 <claire9585@sookmyung.ac.kr>
2022-04-09 13:03:39 +0900

    [feat] PostLike 도메인 생성 (#33)

---
+++ src/main/java/server/sookdak/domain/PostLike.java
@@ -0,0 +1,34 @@
+package server.sookdak.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PostLike {
+
+    @EmbeddedId
+    private UserPostId userPostId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    @MapsId("userId")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    @MapsId("postId")
+    private Post post;
+
+    public static PostLike createPostLike(User user, Post post) {
+        PostLike postLike = new PostLike();
+        postLike.userPostId = new UserPostId(user.getUserId(), post.getPostId());
+        postLike.user = user;
+        postLike.post = post;
+        return postLike;
+    }
+}
