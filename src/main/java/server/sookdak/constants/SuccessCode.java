4656623adca7d271223171b669f85b510264f64b
yujin113 <claire9585@sookmyung.ac.kr>
2022-04-28 11:23:55 +0900

    [feat] 게시글 상세조회 성공코드 상수 작성 (#46)

--- src/main/java/server/sookdak/constants/SuccessCode.java
+++ src/main/java/server/sookdak/constants/SuccessCode.java
@@ -19,6 +19,7 @@ public enum SuccessCode {
     BOARD_DELETE_SUCCESS(OK, "게시판 삭제에 성공했습니다."),
 
     POST_SAVE_SUCCESS(OK, "게시글 저장에 성공했습니다."),
+    POST_DETAIL_READ_SUCCESS(OK, "게시글 상세 조회에 성공했습니다."),
     POST_LIST_READ_SUCCESS(OK, "게시글 목록 조회에 성공했습니다."),
     POST_DELETE_SUCCESS(OK, "게시글 삭제에 성공했습니다."),
 
