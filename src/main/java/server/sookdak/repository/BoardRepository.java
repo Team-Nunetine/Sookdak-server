5e75cd1bd9976d4a80041401989d49834aeb2981
dain0826 <62243967+dain0826@users.noreply.github.com>
2022-05-02 03:05:07 +0900

    [feat] repository에 유저 조건 넣어서 게시판 조회 메서드 추가 (#50)

--- src/main/java/server/sookdak/repository/BoardRepository.java
+++ src/main/java/server/sookdak/repository/BoardRepository.java
@@ -3,12 +3,15 @@ package server.sookdak.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import server.sookdak.domain.Board;
+import server.sookdak.domain.User;
 
 import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board,Long> {
     @Query("SELECT p FROM Board p ORDER BY p.boardId DESC")
     List<Board> findAllDesc();
+
+    List<Board> findByUser(User user);
 
     boolean existsByName(String name);
 }
