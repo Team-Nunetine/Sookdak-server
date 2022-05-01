b9178fa1c6bdfe2a2955f9a07046b57f461e8433
dain0826 <62243967+dain0826@users.noreply.github.com>
2022-05-02 01:40:40 +0900

    [feat] 에러코드 상수 추가 (#48)

--- src/main/java/server/sookdak/constants/ExceptionCode.java
+++ src/main/java/server/sookdak/constants/ExceptionCode.java
@@ -27,6 +27,7 @@ public enum ExceptionCode {
     SOOKMYUNG_ONLY(FORBIDDEN, "숙명 계정으로 로그인해야 합니다."),
     LIKE_DENIED(FORBIDDEN, "본인이 쓴 글은 공감할 수 없습니다."),
     WRITER_ONLY(FORBIDDEN, "본인이 쓴 글만 삭제할 수 있습니다."),
+    SCRAP_DENIED(FORBIDDEN, "본인이 쓴 글은 스크랩할 수 없습니다."),
 
     /* 404 - 찾을 수 없는 리소스 */
     MEMBER_EMAIL_NOT_FOUND(NOT_FOUND, "가입되지 않은 이메일입니다."),
