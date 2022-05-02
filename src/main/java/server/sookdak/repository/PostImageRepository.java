986e6122f256107ad1b5eff4879a6ac01667448c
yujin113 <claire9585@sookmyung.ac.kr>
2022-05-03 01:45:44 +0900

    [feat] PostImageRepository 삭제,조회 메서드 추가 (#53)

--- src/main/java/server/sookdak/repository/PostImageRepository.java
+++ src/main/java/server/sookdak/repository/PostImageRepository.java
@@ -1,10 +1,20 @@
 package server.sookdak.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import server.sookdak.domain.Post;
 import server.sookdak.domain.PostImage;
+
+import java.util.List;
 
 public interface PostImageRepository extends JpaRepository<PostImage, Long> {
 
     boolean existsByPost(Post post);
+
+    @Modifying
+    @Query("delete from PostImage pi where pi.post=?1")
+    void deleteAllByPost(Post post);
+
+    List<PostImage> findAllByPost(Post post);
 }
