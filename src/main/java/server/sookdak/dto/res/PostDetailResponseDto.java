5fca5b1196b711d0557a318eea46ac6080353ba0
yujin113 <claire9585@sookmyung.ac.kr>
2022-05-03 18:00:55 +0900

    [feat] PostList,Detail 응답DTO에 댓글수 추가 (#63)

--- src/main/java/server/sookdak/dto/res/PostDetailResponseDto.java
+++ src/main/java/server/sookdak/dto/res/PostDetailResponseDto.java
@@ -9,15 +9,13 @@ import java.util.List;
 @NoArgsConstructor
 public class PostDetailResponseDto {
     private PostDetail post;
-    private List<CommentList> comments;
 
-    private PostDetailResponseDto(PostDetail post, List<CommentList> comments) {
+    private PostDetailResponseDto(PostDetail post) {
         this.post = post;
-        this.comments = comments;
     }
 
-    public static PostDetailResponseDto of(PostDetail post, List<CommentList> comments) {
-        return new PostDetailResponseDto(post, comments);
+    public static PostDetailResponseDto of(PostDetail post) {
+        return new PostDetailResponseDto(post);
     }
 
     @Getter
@@ -27,34 +25,18 @@ public class PostDetailResponseDto {
         private String createdAt;
         private int likes;
         private int scraps;
+        private int comments;
         private List<String> images;
         private boolean writer;
 
-        public PostDetail(String content, String createdAt, int likes, int scraps, List<String> images, boolean writer) {
+        public PostDetail(String content, String createdAt, int likes, int scraps, int comments, List<String> images, boolean writer) {
             this.content = content;
             this.createdAt = createdAt;
             this.likes = likes;
             this.scraps = scraps;
+            this.comments = comments;
             this.images = images;
             this.writer = writer;
-        }
-    }
-
-    @Getter
-    @NoArgsConstructor
-    public static class CommentList {
-        private Long commentId;
-        private String content;
-        private String createdAt;
-        private int likes;
-        private String image;
-
-        public CommentList(Long commentId, String content, String createdAt, int likes, String image) {
-            this.commentId = commentId;
-            this.content = content;
-            this.createdAt = createdAt;
-            this.likes = likes;
-            this.image = image;
         }
     }
 }
