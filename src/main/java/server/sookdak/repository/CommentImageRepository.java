d225bac7d262fb2f62bcd80ea9deccc1f1209fc5
dain0826 <62243967+dain0826@users.noreply.github.com>
2022-05-04 16:23:47 +0900

    [feat] CommentImageRepository에 findByComment 메서드 추가 (#64)

--- src/main/java/server/sookdak/repository/CommentImageRepository.java
+++ src/main/java/server/sookdak/repository/CommentImageRepository.java
@@ -1,7 +1,12 @@
 package server.sookdak.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import server.sookdak.domain.Comment;
 import server.sookdak.domain.CommentImage;
 
+import java.util.Optional;
+
 public interface CommentImageRepository extends JpaRepository<CommentImage, Long>{
+    Optional<CommentImage> findByComment(Comment comment);
+
 }
