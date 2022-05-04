e28a35832e530dc01cf212bc678143cb415bf472
dain0826 <62243967+dain0826@users.noreply.github.com>
2022-05-04 16:26:23 +0900

    [feat] CommentResponse에 data 없는경우의 생성자추가 (#64)

--- src/main/java/server/sookdak/dto/res/CommentResponse.java
+++ src/main/java/server/sookdak/dto/res/CommentResponse.java
@@ -14,13 +14,25 @@ public class CommentResponse extends BaseResponse {
         this.data = data;
     }
 
+    private CommentResponse(Boolean success, String message){
+        super(success, message);
+    }
+
     public static CommentResponse of(Boolean success, String message, CommentResponseDto data) {
 
         return new CommentResponse(success, message, data);
     }
 
+    public static CommentResponse ofDelete(Boolean success, String message){
+        return new CommentResponse(success, message);
+    }
+
     public static ResponseEntity<CommentResponse> newResponse(SuccessCode code, CommentResponseDto data) {
         CommentResponse response = CommentResponse.of(true, code.getMessage(), data);
         return new ResponseEntity(response, code.getStatus());
+    }
+
+    public static ResponseEntity<CommentResponse> newDeleteResponse(SuccessCode code){
+        return new ResponseEntity(CommentResponse.ofDelete(true, code.getMessage()), code.getStatus());
     }
 }
