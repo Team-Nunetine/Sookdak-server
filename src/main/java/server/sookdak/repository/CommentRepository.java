59c8e30cf372146af4eea4be97abe4f3cc150f08
dain0826 <62243967+dain0826@users.noreply.github.com>
2022-05-10 16:19:18 +0900

    [feat] CommentRepository에 유저가 댓글 단 글 찾는 메서드 추가 (#77)

--- src/main/java/server/sookdak/repository/CommentRepository.java
+++ src/main/java/server/sookdak/repository/CommentRepository.java
@@ -1,8 +1,17 @@
 package server.sookdak.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import server.sookdak.domain.Comment;
+import server.sookdak.domain.Post;
+import server.sookdak.domain.User;
+
+import org.springframework.data.domain.Pageable;
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment,Long> {
+
+    @Query("select distinct c.post from Comment c where c.user=?1 order by c.createdAt desc") //댓글 단 글 중복 방지
+    List<Post> findAllByUserOrderByCreatedAtDesc(User user, Pageable page);
 }
 
