aaa9e4ee70653882552099d5a0a07f6de375e3c9
yujin113 <claire9585@sookmyung.ac.kr>
2022-05-22 11:57:34 +0900

    [feat] 시간표 추가삭제 service 코드 작성 (#89)

--- src/main/java/server/sookdak/service/LectureService.java
+++ src/main/java/server/sookdak/service/LectureService.java
@@ -6,14 +6,28 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import server.sookdak.constants.SuccessCode;
 import server.sookdak.domain.Lecture;
+import server.sookdak.domain.LectureId;
+import server.sookdak.domain.Timetable;
+import server.sookdak.domain.User;
 import server.sookdak.dto.res.lecture.LectureResponseDto;
 import server.sookdak.dto.res.lecture.LectureResponseDto.LectureList;
+import server.sookdak.exception.CustomException;
 import server.sookdak.repository.LectureRepository;
+import server.sookdak.repository.TimetableRepository;
+import server.sookdak.repository.UserRepository;
+import server.sookdak.util.SecurityUtil;
 import server.sookdak.util.WebDriverUtil;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
+
+import static server.sookdak.constants.ExceptionCode.LECTURE_NOT_FOUND;
+import static server.sookdak.constants.ExceptionCode.USER_NOT_FOUND;
+import static server.sookdak.constants.SuccessCode.TIMETABLE_ADD_SUCCESS;
+import static server.sookdak.constants.SuccessCode.TIMETABLE_DELETE_SUCCESS;
 
 @Service
 @Transactional
@@ -21,6 +35,8 @@ import java.util.stream.Collectors;
 public class LectureService {
 
     private final LectureRepository lectureRepository;
+    private final TimetableRepository timetableRepository;
+    private final UserRepository userRepository;
     @Value("${everytime.id}")
     private String id;
     @Value("${everytime.password}")
@@ -33,6 +49,24 @@ public class LectureService {
                 .collect(Collectors.toList());
 
         return LectureResponseDto.of(lectures);
+    }
+
+    public SuccessCode addTimetable(Long lectureId) {
+        String userEmail = SecurityUtil.getCurrentUserEmail();
+        User user = userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
+
+        Lecture lecture = lectureRepository.findById(lectureId)
+                .orElseThrow(() -> new CustomException(LECTURE_NOT_FOUND));
+
+        Optional<Timetable> existTimetable = timetableRepository.findById(new LectureId(user.getUserId(), lecture.getLectureId()));
+        if (existTimetable.isEmpty()) {
+            Timetable timetable = Timetable.createTimetable(user, lecture);
+            timetableRepository.save(timetable);
+            return TIMETABLE_ADD_SUCCESS;
+        }
+        existTimetable.ifPresent(timetableRepository::delete);
+        return TIMETABLE_DELETE_SUCCESS;
     }
 
     public void crawling() throws InterruptedException {
