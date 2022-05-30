d8d6db4b30f12cf85bcca4be28818be9d46234db
yujin113 <claire9585@sookmyung.ac.kr>
2022-05-30 14:42:25 +0900

    [feat] SecurityConfig 쪽지 URI 추가 (#68)

--- src/main/java/server/sookdak/config/SecurityConfig.java
+++ src/main/java/server/sookdak/config/SecurityConfig.java
@@ -47,6 +47,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/post/**").permitAll()
                 .antMatchers("/api/star/**").permitAll()
                 .antMatchers("/api/comment/**").permitAll()
+                .antMatchers("/api/lecture/**").permitAll()
+                .antMatchers("/api/chat/**").permitAll()
+                .antMatchers("/api/message/**").permitAll()
                 .anyRequest().authenticated() //나머지 요청은 모두 인증 필요
 
                 //JwtFilter를 addFilterBefore로 등록했던 JwtSecurityConfig 클래스 적용
