16ec90417d4dcba054666e4076d0f24c6540555f
yujin113 <claire9585@sookmyung.ac.kr>
2022-05-31 18:05:52 +0900

    [feat] 쪽지 목록 조회 위한 repository 메서드 추가 (#107)

--- src/main/java/server/sookdak/repository/MessageRepository.java
+++ src/main/java/server/sookdak/repository/MessageRepository.java
@@ -1,7 +1,11 @@
 package server.sookdak.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import server.sookdak.domain.Message;
+import server.sookdak.domain.MessageRoom;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
+
+    Message findFirstByMessageRoomOrderByCreatedAtDesc(MessageRoom messageRoom);
 }
