d769b4ae322ace95f8e2b30d9ff070006b465aeb
yujin113 <claire9585@sookmyung.ac.kr>
2022-06-01 11:49:36 +0900

    [feat] CommentList 정적 팩토리 메서드 추가 (#108)
    - 삭제된 댓글 객체 반환

--- src/main/java/server/sookdak/dto/res/comment/CommentListResponseDto.java
+++ src/main/java/server/sookdak/dto/res/comment/CommentListResponseDto.java
@@ -1,11 +1,12 @@
 package server.sookdak.dto.res.comment;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import server.sookdak.domain.Comment;
 
+import java.time.LocalDateTime;
 import java.util.List;
-
 
 @Getter
 @NoArgsConstructor
@@ -22,7 +23,7 @@ public class CommentListResponseDto {
 
     @Getter
     @NoArgsConstructor
-    public static class CommentList {
+    public static class CommentList implements Comparable<CommentList> {
         private int commentOrder;
         private Long commentId;
         private Long parent;
@@ -31,11 +32,9 @@ public class CommentListResponseDto {
         private int likes;
         private boolean writer;
         private boolean userLiked;
-        private String createdAt;
+        @JsonFormat(pattern = "yyyy/MM/dd HH:mm", timezone = "Asia/Seoul")
+        private LocalDateTime createdAt;
         private List<CommentList> reply;
-
-
-
 
         public static CommentList of(Comment comment, int commentOrder, boolean writer, boolean userLiked, List<CommentList> reply) {
             CommentList commentList = new CommentList();
@@ -51,13 +50,10 @@ public class CommentListResponseDto {
             commentList.writer = writer;
             commentList.userLiked = userLiked;
             commentList.reply = reply;
-
-
             return commentList;
         }
 
         public static CommentList createReply(Comment comment, int commentOrder, boolean writer, boolean userLiked) {
-
             CommentList commentList = new CommentList();
             commentList.commentOrder = commentOrder;
             commentList.commentId = comment.getCommentId();
@@ -70,10 +66,20 @@ public class CommentListResponseDto {
             commentList.likes = comment.getLikes().size();
             commentList.writer = writer;
             commentList.userLiked = userLiked;
-
             return commentList;
         }
+
+        public static CommentList createNull(List<CommentList> reply, Long commentId) {
+            CommentList commentList = new CommentList();
+            commentList.commentId = commentId;
+            commentList.parent = 0L;
+            commentList.reply = reply;
+            return commentList;
+        }
+
+        @Override
+        public int compareTo(CommentList o) {
+            return Long.compare(commentId, o.commentId);
+        }
     }
-
-
 }
